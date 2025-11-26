83eae6d480cd55cf89cc7e6b51659f56ad5763a6
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-11-26 23:59:59 +0300

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+
   return (
     <div className="app">
       <button className="open-modl-btn">Авторизация</button>
+      {open && (
+        <div className="overlay">
+          <div className="modal">
+            <p>Modal Open!!</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
