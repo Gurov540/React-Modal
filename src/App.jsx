21e9d05000f721420b542cdc20975b0e3a6c41c6
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-11-26 23:55:07 +0300

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <button className="open-modl-btn">Авторизация</button>
+    </div>
+  );
+}
+
+export default App;
