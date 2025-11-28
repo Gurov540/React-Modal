3629f0f1278963642703500b415a9f180a203c17
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-11-29 00:00:01 +0300

    build page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import "./App.css";
+import { Modal } from "./shared/ui/Modal";
 
 function App() {
-  const [open, setOpen] = React.useState(false);
-
   return (
     <div className="app">
-      <button className="open-modl-btn">Авторизация</button>
-      {open && (
-        <div className="overlay">
-          <div className="modal">
-            <p>Modal Open!!</p>
-          </div>
-        </div>
-      )}
+      <Modal></Modal>
     </div>
   );
 }
