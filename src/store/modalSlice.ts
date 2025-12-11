71ef0953734df6109031c58a0511face51a93985
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-12-11 22:26:17 +0300

    create modal slice

---
+++ src/store/modalSlice.ts
@@ -0,0 +1,45 @@
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  ModalNames,
+  SpecificModalDataType,
+} from "../shared/lib/hooks/useModalControl/modalNames";
+
+export type ModalSliceType<
+  T extends keyof typeof ModalNames = keyof typeof ModalNames
+> = T extends infer K
+  ? K extends T
+    ? {
+        modalData: {
+          name: K;
+          value: SpecificModalDataType[K];
+        };
+      }
+    : never
+  : never;
+
+const initialState: ModalSliceType = {
+  modalData: { name: ModalNames.reset, value: undefined },
+};
+
+export const modalSlice = createSlice({
+  name: "modal",
+  initialState,
+  reducers: {
+    setModalData: (
+      state: ModalSliceType,
+      action: PayloadAction<ModalSliceType["modalData"]>
+    ) => {
+      state.modalData = action.payload;
+    },
+  },
+  selectors: {
+    selectModalData: (state) => state.modalData,
+  },
+});
+
+// Action creators
+export const { setModalData } = modalSlice.actions;
+
+// Selectors
+export const { selectModalData } = modalSlice.selectors;
