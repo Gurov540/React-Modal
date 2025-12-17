747e41c07ead242afe18d4b91c504a75a4362905
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-12-17 22:54:46 +0300

    add class name to form

--- src/features/auth/ui/LoginModal/Loginmodal.tsx
+++ src/features/auth/ui/LoginModal/Loginmodal.tsx
@@ -1,5 +1,7 @@
-import { Modal } from "../../../../shared/ui/Modal/Modal";
 import { useModalControl } from "../../../../shared/lib/hooks/useModalControl/useModalControl";
+import styles from "./LoginModal.module.css";
+import { Modal } from "../../../../shared/ui/Modal/Modal";
+import { Input } from "../../../../shared/ui/Input";
 
 export const LoginModal = () => {
   const {
@@ -13,9 +15,9 @@ export const LoginModal = () => {
     <Modal isOpen={isOpen} onClose={closeModal}>
       <h2>Вход</h2>
 
-      <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Пароль" />
+      <form className={styles.loginForm}>
+        <Input type="email" placeholder="Login" fullWidth></Input>
+        <Input type="password" placeholder="password" fullWidth></Input>
 
         <button type="submit">Войти</button>
       </form>
