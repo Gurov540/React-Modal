de7c6d3d7d4d4707f7176accd3eaa590566a69bf
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-12-18 22:39:08 +0300

    add hero section to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import styles from "./HomePage.module.css";
 import { Header } from "../../widgets/Header";
+import { HeroSection } from "../../widgets/HomePage-Sections/HeroSection/HeroSection";
 
 export const HomePage = () => {
   return (
-    <>
+    <div className={styles.homePage}>
       <Header></Header>
-    </>
+      <HeroSection></HeroSection>
+    </div>
   );
 };
