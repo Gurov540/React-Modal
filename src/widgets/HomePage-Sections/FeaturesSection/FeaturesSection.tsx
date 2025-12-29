2249b9ca1bbffcc3cff1f6fd59149b8df26ddc03
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-12-29 21:06:31 +0300

    init repo

---
+++ src/widgets/HomePage-Sections/FeaturesSection/FeaturesSection.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./HeroSection.module.css";
+
+export const FeaturesSection: React.FC = () => {
+  return (
+    <>
+      <section className={styles.FeaturesSection}></section>
+    </>
+  );
+};
